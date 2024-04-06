5b8f907dc98ba7c3e572c30e9325441de6b0502d
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-06 18:39:14 -0300

    feat: add service and user controller on my-car-value folder

--- my-car-value/dist/users/users.controller.js
+++ my-car-value/dist/users/users.controller.js
@@ -15,9 +15,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersController = void 0;
 const common_1 = require("@nestjs/common");
 const create_user_dto_1 = require("./dtos/create-user.dto");
+const users_service_1 = require("./users.service");
+const update_user_dto_1 = require("./dtos/update-user.dto");
 let UsersController = class UsersController {
+    constructor(userService) {
+        this.userService = userService;
+    }
     createUser(body) {
-        console.log(body);
+        this.userService.create(body.email, body.password);
+    }
+    async findUser(id) {
+        const user = await this.userService.findOne(parseInt(id));
+        if (!user)
+            throw new common_1.NotFoundException('User Not Found');
+        return user;
+    }
+    findAllUsers(email) {
+        return this.userService.find(email);
+    }
+    updateUser(id, body) {
+        return this.userService.update(parseInt(id), body);
+    }
+    removeUser(id) {
+        return this.userService.remove(parseInt(id));
     }
 };
 exports.UsersController = UsersController;
@@ -28,7 +48,37 @@ __decorate([
     __metadata("design:paramtypes", [create_user_dto_1.CreateUserDto]),
     __metadata("design:returntype", void 0)
 ], UsersController.prototype, "createUser", null);
+__decorate([
+    (0, common_1.Get)('/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], UsersController.prototype, "findUser", null);
+__decorate([
+    (0, common_1.Get)(),
+    __param(0, (0, common_1.Query)('email')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], UsersController.prototype, "findAllUsers", null);
+__decorate([
+    (0, common_1.Patch)('/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, update_user_dto_1.UpdateUserDto]),
+    __metadata("design:returntype", void 0)
+], UsersController.prototype, "updateUser", null);
+__decorate([
+    (0, common_1.Delete)('/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], UsersController.prototype, "removeUser", null);
 exports.UsersController = UsersController = __decorate([
-    (0, common_1.Controller)('auth')
+    (0, common_1.Controller)('auth'),
+    __metadata("design:paramtypes", [users_service_1.UsersService])
 ], UsersController);
 //# sourceMappingURL=users.controller.js.map
--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,10 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   public createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  public async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+
+    if (!user) throw new NotFoundException('User Not Found');
+
+    return user;
+  }
+
+  @Get()
+  public findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  public updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  public removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
