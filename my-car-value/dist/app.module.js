cbc0ec61bc55372a96dc489d24ce6fcb8972f87e
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-11 16:45:11 -0300

    feat: add env configs files

--- my-car-value/dist/app.module.js
+++ my-car-value/dist/app.module.js
@@ -15,23 +15,41 @@ const users_module_1 = require("./users/users.module");
 const reports_module_1 = require("./reports/reports.module");
 const user_entity_1 = require("./users/user.entity");
 const report_entity_1 = require("./reports/report.entity");
+const core_1 = require("@nestjs/core");
+const config_1 = require("@nestjs/config");
+const cookieSession = require('cookie-session');
 let AppModule = class AppModule {
+    configure(consumer) {
+        consumer.apply(cookieSession({ keys: ['cookie-keys'] })).forRoutes('*');
+    }
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            typeorm_1.TypeOrmModule.forRoot({
-                type: 'sqlite',
-                database: 'db.sqlite',
-                entities: [user_entity_1.User, report_entity_1.Report],
-                synchronize: true,
+            config_1.ConfigModule.forRoot({
+                isGlobal: true,
+                envFilePath: `.env.${process.env.NODE_ENV}`,
+            }),
+            typeorm_1.TypeOrmModule.forRootAsync({
+                inject: [config_1.ConfigService],
+                useFactory: (config) => {
+                    return {
+                        type: 'sqlite',
+                        database: config.get('DB_NAME'),
+                        synchronize: true,
+                        entities: [user_entity_1.User, report_entity_1.Report],
+                    };
+                },
             }),
             users_module_1.UsersModule,
             reports_module_1.ReportsModule,
         ],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        providers: [
+            app_service_1.AppService,
+            { provide: core_1.APP_PIPE, useValue: new common_1.ValidationPipe({ whitelist: true }) },
+        ],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
